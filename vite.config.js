a26a36ce82fa1660646ad90e671163e46bd93b1d
DESKTOP-30E6PLF\BOSSRT-LNB <rujiroj2537@gmail.com>
2022-02-25 15:23:50 +0700

    fix base add /

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'myresume',
+  base:'/myresume/',
   plugins: [vue()],
   resolve: {
     alias: {
