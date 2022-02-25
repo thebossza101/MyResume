ef60fa18844eac435bfb2e333c676a72d908d733
DESKTOP-30E6PLF\BOSSRT-LNB <rujiroj2537@gmail.com>
2022-02-25 20:58:01 +0700

    test deploy

--- src/components/layout/Hero.vue
+++ src/components/layout/Hero.vue
@@ -2,7 +2,7 @@
       <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex flex-column justify-content-center">
     <div class="container" data-aos="zoom-in" data-aos-delay="100">
-      <h1>Mr. Rujiroj Tamnitra</h1>
+      <h1>Mr. Rujiroj Tamnitra 5555</h1>
       <p>I'm <span class="typed" data-typed-items="Web Developer, Software Engineer, Freelancer"></span></p>
       <div class="social-links">
         <a href="https://twitter.com/thebossza101" class="twitter"><i class="bx bxl-twitter"></i></a>
